7c3c47412eece0bd52f2bbb158c0653f90d035d8
Glauber Ferreira <glauberdnb@gmail.com>
2025-09-08 17:27:40 +0000

    barbeiros no menu não está conseguindo acessar

--- src/components/dashboard-layout.tsx
+++ src/components/dashboard-layout.tsx
@@ -65,7 +65,7 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
           <SidebarMenu>
             {navItems.map((item) => (
               <SidebarMenuItem key={item.href}>
-                <Link href={item.href} legacyBehavior passHref>
+                <Link href={item.href} passHref>
                   <SidebarMenuButton
                     isActive={pathname === item.href}
                     tooltip={item.label}
